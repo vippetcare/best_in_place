2f64882727acffd85451484c4b9d2a39427a1944
H. Michael Ko <hyosunko@gmail.com>
2019-10-11 07:49:37 +0900

    add `Action:View::LookupContext` to fix deprecated warning

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -1,7 +1,7 @@
 module BestInPlace
   class Railtie < Rails::Railtie
     initializer "set view helpers" do
-      BestInPlace::ViewHelpers = ActionView::Base.new
+      BestInPlace::ViewHelpers = ActionView::Base.new(ActionView::LookupContext.new(nil), {}, nil)
     end
   end
 end
